ae9bde3d76294b7d54964ed0bef5fde6870cbc5d
euxn23 <euxn23@gmail.com>
2020-01-10 23:51:28 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>陽光</p>
+        <a
+          href="https://docs.google.com/forms/d/1LV16h3zKvS6FC_K3ngi6NS96AncpC6kEC78s8-JQEas"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Join the server
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
